d4f106753452fed1f09a3e68b999707cbd87253c
Mr Yang <415646472@qq.com>
2016-02-03 16:25:35 +0800

    no message

---
+++ TYWaterWaveView-master/TYWaveProgressDemo/TYWaveProgressView/TYWaveProgressView.m
@@ -0,0 +1,133 @@
+//
+//  TYWaveProgressDemo.m
+//  TYWaveProgressDemo
+//
+//  Created by tanyang on 15/4/14.
+//  Copyright (c) 2015年 tanyang. All rights reserved.
+//
+
+#import "TYWaveProgressView.h"
+#import "TYWaterWaveView.h"
+
+@interface TYWaveProgressView ()
+@property (nonatomic, weak,readwrite) UIImageView *backgroundImageView;
+@property (nonatomic, weak) TYWaterWaveView *waterWaveView;
+@property (nonatomic, weak,readwrite) UILabel *numberLabel;
+@property (nonatomic, weak,readwrite) UILabel *explainLabel;
+@property (nonatomic, weak,readwrite) UILabel *unitLabel;
+@end
+
+@implementation TYWaveProgressView
+
+-(instancetype)initWithFrame:(CGRect)frame
+{
+    if (self = [super initWithFrame:frame]) {
+        [self setupView];
+    }
+    return self;
+}
+
+- (id)initWithCoder:(NSCoder *)aDecoder
+{
+    if (self = [super initWithCoder:aDecoder]) {
+        [self setupView];
+    }
+    return self;
+}
+
+- (void)setupView
+{
+    self.backgroundColor=[UIColor blueColor];
+    [self addBackgroudImageView];
+    [self addWaterWaveView];
+    [self addNumberlabel];
+    [self addUnitLabel];
+    [self addExplainLabel];
+}
+//添加水池背景图片
+- (void)addBackgroudImageView
+{
+//    UIImageView *backgroundImageView = [[UIImageView alloc]init];
+//    [self addSubview:backgroundImageView];
+//    _backgroundImageView = backgroundImageView;
+}
+//添加水池波浪视图
+- (void)addWaterWaveView
+{
+    TYWaterWaveView *waterWaveView = [[TYWaterWaveView alloc]init];
+    [self addSubview:waterWaveView];
+    _waterWaveView = waterWaveView;
+}
+//添加水池百分比label
+- (void)addNumberlabel
+{
+    UILabel *numberLabel = [[UILabel alloc]init];
+    numberLabel.textAlignment = NSTextAlignmentCenter;
+    [self addSubview:numberLabel];
+    _numberLabel = numberLabel;
+}
+//添加％label
+- (void)addUnitLabel
+{
+//    UILabel *unitLabel = [[UILabel alloc]init];
+//    [self addSubview:unitLabel];
+//    _unitLabel = unitLabel;
+}
+//添加解释label
+- (void)addExplainLabel
+{
+//    UILabel *explainLabel = [[UILabel alloc]init];
+//    explainLabel.textAlignment = NSTextAlignmentCenter;
+//    [self addSubview:explainLabel];
+//    _explainLabel = explainLabel;
+    
+}
+
+- (void)startWave
+{
+    if (_numberLabel.text) {
+        if (_percent > 0) {
+            _waterWaveView.percent = _percent;
+            [_waterWaveView startWave];
+        }else{
+            [self resetWave];
+        }
+    }
+}
+
+- (void)resetWave
+{
+    [_waterWaveView reset];
+}
+
+- (void)layoutSubviews
+{
+    [super layoutSubviews];
+    
+    CGFloat viewHeight = CGRectGetHeight(self.frame);
+    CGFloat viewWidth = CGRectGetWidth(self.frame);
+    //设置背景图片frame
+    _backgroundImageView.frame = self.bounds;
+    //设置波浪frame
+    _waterWaveView.frame = CGRectMake(_waveViewMargin.left, _waveViewMargin.top, viewWidth-_waveViewMargin.left-_waveViewMargin.right, viewHeight - _waveViewMargin.top - _waveViewMargin.bottom);
+    //设置圆角
+    _waterWaveView.layer.cornerRadius = MIN(CGRectGetHeight(_waterWaveView.frame)/2, CGRectGetWidth(_waterWaveView.frame)/2);
+    
+    CGFloat numberLabelWidth = viewWidth * 2/3;
+    CGFloat numberLabelHeight = _numberLabel.font.pointSize + 2;
+    
+    CGFloat explainLabelWidth = viewWidth * 3/4;
+    CGFloat explainLabelHeight = _explainLabel.font.pointSize;
+    
+    _numberLabel.frame = CGRectMake((viewWidth - numberLabelWidth)/2, (viewHeight - numberLabelHeight - explainLabelHeight)/2, numberLabelWidth, numberLabelHeight);
+    
+    if (_unitLabel.text.length > 0) {
+        _unitLabel.frame = CGRectMake(viewWidth * 0.7, CGRectGetMinY(_numberLabel.frame)*1.2, _unitLabel.font.pointSize*3, _unitLabel.font.pointSize);
+    } else{
+        _unitLabel.frame = CGRectZero;
+    }
+    
+    _explainLabel.frame = CGRectMake((viewWidth - explainLabelWidth)/2, CGRectGetMaxY(_numberLabel.frame)-numberLabelHeight/30, explainLabelWidth, explainLabelHeight);
+}
+
+@end
---
+++ TYWaterWaveView-master/TYWaveProgressDemo/ViewController.m
@@ -0,0 +1,78 @@
+//
+//  ViewController.m
+//  TYWaveProgressDemo
+//
+//  Created by tanyang on 15/4/15.
+//  Copyright (c) 2015年 tanyang. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "TYWaveProgressView.h"
+
+@interface ViewController ()
+@property (nonatomic, weak) TYWaveProgressView *waveProgressView1;
+@property (nonatomic, weak) TYWaveProgressView *waveProgressView2;
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor=[UIColor grayColor];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    [self addWaveProgressView1];
+
+//    [self addWaveProgressView2];
+}
+
+- (void)addWaveProgressView1
+{
+    TYWaveProgressView *waveProgressView = [[TYWaveProgressView alloc]initWithFrame:CGRectMake((CGRectGetWidth(self.view.frame) - 180)/2, 44, 180, 180)];
+    waveProgressView.waveViewMargin = UIEdgeInsetsMake(15, 15, 20, 20);
+    waveProgressView.backgroundImageView.image = [UIImage imageNamed:@"bg_tk_003"];
+    waveProgressView.numberLabel.text = @"0.9";
+    waveProgressView.numberLabel.font = [UIFont boldSystemFontOfSize:70];
+    waveProgressView.numberLabel.textColor = [UIColor whiteColor];
+    waveProgressView.unitLabel.text = @"%";
+    waveProgressView.unitLabel.font = [UIFont boldSystemFontOfSize:20];
+    waveProgressView.unitLabel.textColor = [UIColor whiteColor];
+    waveProgressView.explainLabel.text = @"水平";
+    waveProgressView.explainLabel.font = [UIFont systemFontOfSize:20];
+    waveProgressView.explainLabel.textColor = [UIColor whiteColor];
+    
+    waveProgressView.percent = 0.50;
+    [self.view addSubview:waveProgressView];
+    _waveProgressView1 = waveProgressView;
+    [_waveProgressView1 startWave];
+}
+
+- (void)addWaveProgressView2
+{
+    TYWaveProgressView *waveProgressView = [[TYWaveProgressView alloc]initWithFrame:CGRectMake((CGRectGetWidth(self.view.frame) - 180)/2, 300, 180, 180)];
+    waveProgressView.waveViewMargin = UIEdgeInsetsMake(15, 15, 20, 20);
+    waveProgressView.backgroundImageView.image = [UIImage imageNamed:@"bg_tk_003"];
+    waveProgressView.numberLabel.text = @"68";
+    waveProgressView.numberLabel.font = [UIFont boldSystemFontOfSize:70];
+    waveProgressView.numberLabel.textColor = [UIColor whiteColor];
+    waveProgressView.explainLabel.text = @"评分";
+    waveProgressView.explainLabel.font = [UIFont systemFontOfSize:20];
+    waveProgressView.explainLabel.textColor = [UIColor whiteColor];
+    
+    waveProgressView.percent = 0.68;
+    [self.view addSubview:waveProgressView];
+    _waveProgressView2 = waveProgressView;
+    [_waveProgressView2 startWave];
+
+}
+- (IBAction)reStartWave:(id)sender {
+    [_waveProgressView1 startWave];
+    [_waveProgressView2 startWave];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
